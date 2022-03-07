d1836a9316102196a4fc2fcc71720b023675d609
阏男秀 <ynanxiu@qq.com>
2022-03-07 16:38:36 +0800

    feat: 添加 simpleui 示例

---
+++ 110-simpleui/proj/settings_simpleui.py
@@ -0,0 +1,11 @@
+from .settings import INSTALLED_APPS
+
+LANGUAGE_CODE = 'zh-Hans'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+INSTALLED_APPS = [
+    'simpleui',
+] + INSTALLED_APPS
+
+SIMPLEUI_HOME_INFO = False
