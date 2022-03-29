bf97f363f6dff50142003ebaca60f650eb994f57
阏男秀 <ynanxiu@qq.com>
2022-03-29 10:31:33 +0800

    refactor: 更新 django-river-utils 模块的流程历史记录

--- 116-ticket/packages/django-river-utils/river_utils/tables/transitionapproval.py
+++ 116-ticket/packages/django-river-utils/river_utils/tables/transitionapproval.py
@@ -8,10 +8,12 @@ class TransitionApprovalTable(tables.Table):
 
     transaction_date = tables.Column(verbose_name=_('日期/时间'))
     transactioner = tables.Column(verbose_name=_('用户'))
-    result = tables.Column(verbose_name=_('处理结果'), accessor='transition__destination_state__label')
+    operation = tables.Column(verbose_name=_('操作'), accessor='name')
+    # comment = tables.Column(verbose_name=_('批注'), accessor='comment')
+    # result = tables.Column(verbose_name=_('处理结果'), accessor='transition__destination_state__label')
 
     class Meta:
         model = TransitionApproval
         template_name = 'django_tables2/semantic.html'
-        fields = ('transaction_date', 'transactioner', 'result',)
+        fields = ('transaction_date', 'transactioner', 'operation',)
         attrs = {'style': 'width:100%;'}
