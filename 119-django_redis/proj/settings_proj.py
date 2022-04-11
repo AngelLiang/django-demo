052bfd069939679c7444cf63e96f31ece331c2e9
阏男秀 <ynanxiu@qq.com>
2022-04-11 15:08:33 +0800

    refactor: 更新 django_redis 配置变量

--- 119-django_redis/proj/settings_proj.py
+++ 119-django_redis/proj/settings_proj.py
@@ -8,14 +8,10 @@ TIME_ZONE = 'Asia/Shanghai'
 INSTALLED_APPS = [
 ] + INSTALLED_APPS
 
-REDIS_SCHEME = os.getenv('REDIS_SCHEME', 'redis')
-REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = os.getenv('REDIS_PORT', '6379')
-REDIS_DB = os.getenv('REDIS_DB', '0')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"{REDIS_SCHEME}://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",
+        "LOCATION": os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
