f08b8c5b668550221b7867eb3b95bfc3b4e346a5
阏男秀 <ynanxiu@qq.com>
2022-04-18 11:31:28 +0800

    refactor: 添加 setup.py

---
+++ 120-import_item/import_item/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='import_item',  # 包名称
+    version='0.1.0',  # 版本
+    license='MIT',
+    description='明细导入功能',
+    platforms='any',
+    zip_safe=False,
+    include_package_data=True,
+)
