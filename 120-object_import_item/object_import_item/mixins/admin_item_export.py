d2e29ea44abc985e16d9d48a4265df504879d643
AngelLiang <pl01665077@163.com>
2022-04-18 23:30:31 +0800

    feat: object_import_item示例添加下载导入模板功能

--- 120-object_import_item/object_import_item/mixins/admin_item_export.py
+++ 120-object_import_item/object_import_item/mixins/admin_item_export.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from import_export.signals import post_import
 from django.urls import path
@@ -14,10 +15,24 @@ class ItemExportAdminMixin:
         urls = super().get_urls()
         my_urls = [
             path('import-template/',
-                 self.admin_site.admin_view(self.export_action),
+                 self.admin_site.admin_view(self.import_template),
                  name='%s_%s_import_template' % self.get_model_info()),
         ]
         return my_urls + urls
+
+    def import_template(self, request, *args, **kwargs):
+        formats = self.get_export_formats()
+        file_format = formats[0]()
+
+        queryset = self.model.objects.none()
+        export_data = self.get_template_data(file_format, queryset, request=request)
+
+        content_type = file_format.get_content_type()
+        response = HttpResponse(export_data, content_type=content_type)
+        response['Content-Disposition'] = 'attachment; filename="{}-template.{}"'.format(
+            self.model.__name__, file_format.get_extension()
+        )
+        return response
 
     def export_action(self, request, *args, **kwargs):
         self.request = request
@@ -25,9 +40,19 @@ class ItemExportAdminMixin:
 
     def get_export_queryset(self, request):
         is_template = request.GET.get('_is_template', None)
-        print(is_template)
         if is_template:
             return self.model.objects.none()
         master_id = request.GET.get('master_id')
         queryset = super().get_export_queryset(request)
         return queryset
+
+    def get_template_data(self, file_format, queryset, *args, **kwargs):
+        """
+        Returns file_format representation for given queryset.
+        """
+        request = kwargs.pop("request")
+
+        resource_class = self.get_export_resource_class()
+        data = resource_class(**self.get_export_resource_kwargs(request)).export(queryset, *args, **kwargs)
+        export_data = file_format.export_data(data)
+        return export_data
