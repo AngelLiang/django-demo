5e441d5853512aee3e7690f67377561b85762d22
AngelLiang <pl01665077@163.com>
2023-08-11 21:27:42 +0800

    feat: 添加 134-django_ninja 示例

---
+++ 134-django_ninja/README.md
@@ -0,0 +1,9 @@
+# django-ninja示例
+
+启动服务
+
+```
+python manage.py runserver
+```
+
+启动服务后，访问 http://127.0.0.1:8000/api/docs 查看文档
---
+++ 134-django_ninja/proj/settings_proj.py
@@ -0,0 +1,28 @@
+import os
+
+LANGUAGE_CODE = 'zh-Hans'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+]
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Basic': {
+            'type': 'basic'
+        },
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
