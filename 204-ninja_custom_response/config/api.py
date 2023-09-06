a8b71fcba50b48ffda34204e9d6ebff989dc9852
阏男秀 <ynanxiu@qq.com>
2023-09-06 09:10:50 +0800

    refactor: 更新 ninja_custom_response

--- 204-ninja_custom_response/config/api.py
+++ 204-ninja_custom_response/config/api.py
@@ -6,6 +6,7 @@ from ninja.renderers import BaseRenderer
 from ninja import Schema, ModelSchema
 from ninja import Field
 
+
 class ORJSONRenderer(BaseRenderer):
     media_type = "application/json"
 
@@ -36,53 +37,79 @@ class UserSchema(ModelSchema):
         model_fields = ['id', 'username', 'first_name', 'last_name']
 
 
-class UserListOut(Schema):
-    class Data(Schema):
-        records: List[UserSchema]
-        total: int
-
-    code :int
-    message: str
-    data: Data
-
-
-# def make_response_schema(class_name:str, data_schema):
-#     # 创建Data内部类
-#     # 先创建一个简单的Data类
+# class UserListOut(Schema):
 #     class Data(Schema):
-#         pass
-    
-#     # 动态地给Data添加字段
-#     Data.__annotations__ = {
-#         'records': List[data_schema],
-#         'total': int
-#     }
-#     Data.records = List[data_schema]
-#     Data.total = int
+#         records: List[UserSchema]
+#         total: int
 
-#     # # 创建外部类并添加Data作为其属性
-#     return type(
-#         class_name, 
-#         (Schema,), 
-#         {
-#             'code': int,
-#             'message': str,
-#             'data': Data
-#         }
-#     )
+#     code :int
+#     message: str
+#     data: Data
 
-# UserListOut = make_response_schema('UserListOut', UserSchema)
+
+def make_records_response_schema(class_name: str, model_schema):
+    """
+    创建一个如下的类
+
+    from ninja import Schema
+
+    class UserListOut(Schema):
+        class Data(Schema):
+            records: List[UserSchema]
+            total: int
+
+        code :int = 0
+        message: str = 'success'
+        data: Data
+
+    """
+    from pydantic import create_model
+
+    data_schema_fields = {
+        'records': (List[model_schema], None),
+        'total': (int, None)
+    }
+    data_schema = create_model(
+            'Data',
+            __config__=None,
+            __base__=Schema,
+            __module__=Schema.__module__,
+            __validators__={},
+            **data_schema_fields,
+        )  # type: ignore
+
+    response_schema_fields = {
+        'code': (int, 0),
+        'message': (str, 'success'),
+        'data': (data_schema, None)
+    }
+    response_schema = create_model(
+        class_name,
+        __config__=None,
+        __base__=Schema,
+        __module__=Schema.__module__,
+        __validators__={},
+        **response_schema_fields
+    )
+
+    return response_schema
+
+UserListOut = make_records_response_schema('UserListOut', UserSchema)
+
+
+def make_records_response(records, total, code=0, message='success'):
+    return {
+        'code': code,
+        'message':message,
+        'data': {
+            'records': records,
+            'total': total
+        }
+    }
 
 @api.get('/users', response=UserListOut)
 def list_users(request):
     user_list = User.objects.all()
     user_total = user_list.count()
     records = [UserSchema.from_orm(user).dict() for user in user_list]
-    return {
-        'code': 0,
-        'message':'success',
-        'data': {
-            'records': records,
-            'total': user_total
-        }
-    }
+    return make_records_response(records, user_total)
