763f8abddd72835176ab99d01aea36bd2a0ca462
阏男秀 <ynanxiu@qq.com>
2023-09-08 09:19:27 +0800

    feat: add ninja_token

---
+++ 210-ninja_token/config/api.py
@@ -0,0 +1,17 @@
+from ninja import NinjaAPI
+from ninja.security import HttpBearer
+
+
+api = NinjaAPI()
+
+
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "supersecret":
+            return token
+
+
+@api.get("/bearer", auth=AuthBearer())
+def bearer(request):
+    return {"token": request.auth}
+
