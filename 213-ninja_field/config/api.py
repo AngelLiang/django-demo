a67fbd69122a1603fc7f4974b97a7eab977d99a8
阏男秀 <ynanxiu@qq.com>
2023-09-15 11:25:12 +0800

    refactor: 重构 ninja_field 示例

--- 213-ninja_field/config/api.py
+++ 213-ninja_field/config/api.py
@@ -8,10 +8,13 @@ class HelloIn(Schema):
     each_string:str = Field(alias='eachString')
 
 class HelloOut(Schema):
-    eachString:str = Field(alias='each_string')
+    each_string:str = Field(alias='eachString')
 
+    class Config:
+        # 修改响应字段为驼峰需要标记这个
+        allow_population_by_field_name = True 
 
-@api.post("/hello", response=HelloOut)
+@api.post("/hello", response=HelloOut, by_alias=True)  # 修改响应字段为驼峰需要标记 by_alias 为 True
 def hello(request, hello:HelloIn):
     return {
         'each_string': hello.each_string
