03f71a850d8d8cddb6adadf66a542e8c80a76aa2
AngelLiang <pl01665077@163.com>
2023-10-03 00:17:17 +0800

    feat: 添加用户登录后的详情页面

---
+++ 217-django_allauth_demo/user_profile/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+    user = request.user
+    return render(request, 'user_profile/profile.html', {'user': user})
