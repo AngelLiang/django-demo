ec9db908d9c1abdab9d217727459e78533b02256
AngelLiang <pl01665077@163.com>
2023-10-17 22:59:03 +0800

    fix: fix django_zipapp_demo

--- 219-django_zipapp_demo/config/settings.py
+++ 219-django_zipapp_demo/config/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
