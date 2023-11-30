76d2839166ff0b53e499a8a290b151ee29e39e7c
阏男秀 <ynanxiu@qq.com>
2023-11-30 11:54:49 +0800

    update: update 218-ninja_utils/utils/services

--- 218-ninja_utils/utils/services.py
+++ 218-ninja_utils/utils/services.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 from utils.errors import NotFoundError
 
 
@@ -28,9 +28,20 @@ class BaseService:
             obj_total = obj_list.count()
         return obj_list, obj_total
 
-    def get_list_and_total(self, params: Dict = None):
+    def filter_by_params(self, qs, params, filter_keys):
+        if filter_keys is None:
+            return qs
+        for key in filter_keys:
+            value = params.get(key)
+            if value is not None:
+                filter_params = {key: value}
+                qs = qs.filter(**filter_params)
+        return qs
+
+    def get_list_and_total(self, params: Dict = None, filter_keys: List | None = None):
         qs = self.get_queryset()
         obj_list, obj_total = self.paginate_queryset(qs, params)
+        obj_list = self.filter_by_params(obj_list, params, filter_keys)
         records = [self.ListOut.from_orm(record).dict() for record in obj_list]
         return records, obj_total
 
@@ -45,7 +56,7 @@ class BaseService:
         obj = self.get_by_id(id)
         return self.DetailOut.from_orm(obj).dict()
 
-    def update_by_id(self, id, data) -> Dict:
+    def update_by_id(self, id, data):
         qs = self.get_queryset()
         obj = qs.filter(id=id).first()
         if not obj:
@@ -53,8 +64,7 @@ class BaseService:
         for attr, val in data.items():
             # getattr(obj, attr, None)
             setattr(obj, attr, val)
-        obj.save()
-        return True
+        return obj.save()
 
     def delete_by_id(self, id):
         qs = self.get_queryset()
