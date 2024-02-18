7ea322fd431611e5a099d5b4146c80676fc8aa41
AngelLiang <pl01665077@163.com>
2024-02-18 11:18:15 +0800

    refactor: updated ninja_utils

--- 218-ninja_utils/utils/schemas.py
+++ 218-ninja_utils/utils/schemas.py
@@ -9,7 +9,9 @@ class ResponseOut(Schema):
     data: dict
 
 
-def make_response_schema(class_name: str, data_schema):
+def make_response_schema(data_schema: object, class_name: str = None):
+    if not class_name:
+        class_name = data_schema.__name__ + 'Response'
     response_schema_fields = {
         'code': (int, 0),
         'message': (str, 'success'),
@@ -26,14 +28,14 @@ def make_response_schema(class_name: str, data_schema):
     return response_schema
 
 
-def make_records_response_schema(class_name: str, model_schema):
+def make_records_response_schema(model_schema: object, class_name: str = None):
     """
     创建一个如下的类
 
     from ninja import Schema
 
     class UserListOut(Schema):
-        class Data(Schema):
+        class UserListOutData(Schema):
             records: List[UserSchema]
             total: int
             current: int
@@ -44,35 +46,23 @@ def make_records_response_schema(class_name: str, model_schema):
         data: Data
 
     """
-
+    if not class_name:
+        class_name = model_schema.__name__ + 'ListResponse'
     data_schema_fields = {
         'records': (List[model_schema], None),
         'total': (int, None),
         'current': (int, None),
         'size': (int, None),
     }
+    data_schema_name = class_name + 'Data'
     data_schema = create_model(
-        'Data',
+        data_schema_name,
         __config__=None,
         __base__=Schema,
         __module__=Schema.__module__,
         __validators__={},
         **data_schema_fields,
     )  # type: ignore
-
-    # response_schema_fields = {
-    #     'code': (int, 0),
-    #     'message': (str, 'success'),
-    #     'data': (data_schema, None)
-    # }
-    # response_schema = create_model(
-    #     class_name,
-    #     __config__=None,
-    #     __base__=Schema,
-    #     __module__=Schema.__module__,
-    #     __validators__={},
-    #     **response_schema_fields
-    # )
-    response_schema = make_response_schema(class_name, data_schema)
+    response_schema = make_response_schema(data_schema, class_name)
 
     return response_schema
