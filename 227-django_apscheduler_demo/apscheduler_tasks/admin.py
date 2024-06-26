abc804e571dd0dd783e45f3e57f2448e4bae92bf
AngelLiang <pl01665077@163.com>
2024-06-26 16:39:45 +0800

    feat: update 227-django_apscheduler_demo/apscheduler_tasks/admin.py

--- 227-django_apscheduler_demo/apscheduler_tasks/admin.py
+++ 227-django_apscheduler_demo/apscheduler_tasks/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django_apscheduler import util
 from django.utils.safestring import mark_safe
+from django.utils import timezone
+from django.conf import settings
+import pytz
 
 from django_apscheduler.models import DjangoJob, DjangoJobExecution
 from django_apscheduler.admin import DjangoJobAdmin as _DjangoJobAdmin, DjangoJobExecutionAdmin as _DjangoJobExecutionAdmin
@@ -20,7 +23,14 @@ class DjangoJobAdmin(_DjangoJobAdmin):
         except DjangoJobExecution.DoesNotExist:
             return "None"
 
-    average_duration.short_description = "评价运行用时（秒）"
+    def local_run_time(self, obj):
+        dt = obj.next_run_time
+        if dt and settings.USE_TZ and timezone.is_aware(dt):
+            dt = timezone.localtime(dt)
+        return dt or "(paused)"
+
+    average_duration.short_description = "平均运行用时（秒）"
+    local_run_time.short_description = '下一次运行时间'
 
 
 @admin.register(DjangoJobExecution)
@@ -34,11 +44,14 @@ class DjangoJobExecutionAdmin(_DjangoJobExecutionAdmin):
         )
 
     def local_run_time(self, obj):
-        return util.get_local_dt_format(obj.run_time)
+        dt = obj.run_time
+        if dt and settings.USE_TZ and timezone.is_aware(dt):
+            dt = timezone.localtime(dt)
+        return dt
 
     def duration_text(self, obj):
         return obj.duration or "N/A"
 
     html_status.short_description = '状态'
     duration_text.short_description = '执行时间（秒）'
-    local_run_time.short_description = '本地运行时间'
+    local_run_time.short_description = '运行时间'
